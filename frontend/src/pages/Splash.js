f1dcc57ae9bf70761ebc59402cecc6040224fe77
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-03-09 20:59:22 +0200

    Update Splash.js

--- frontend/src/pages/Splash.js
+++ frontend/src/pages/Splash.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Splash() {
   return (
     <div>
-      <h1 className="text-red-500 text-7xl">Splash!!</h1>
+      <h1 className="text-red-500 text-5xl">Splash!!</h1>
     </div>
   );
 }
