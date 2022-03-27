a5697d955ab5933274e50a234989f9e023a57116
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-03-27 05:38:01 +0200

    navigate to signup

--- frontend/src/pages/getstarted/index.js
+++ frontend/src/pages/getstarted/index.js
@@ -3,7 +3,17 @@ import Button from "../../components/library/Button";
 import Card from "../../components/library/Card";
 import Logo from "../../components/SVG/Logo";
 import Header from "./Header";
+import { useNavigate } from "react-router-dom";
 export default function GetStarted() {
+  const navigate = useNavigate();
+  const getStartedForPatient = () => {
+    navigate("/signup");
+    //todo dispatch action to set user type to patient
+  };
+  const getStartedForCaretaker = () => {
+    navigate("/signup");
+    //todo dispatch action to set user type to caretaker
+  };
   return (
     <div className="pt-10 h-screen font-mon flex flex-col gap-5 2xs:gap-10 items-center  ">
       <Logo />
@@ -14,7 +24,7 @@ export default function GetStarted() {
             <p className="text-sec font-semibold pb-4">
               I'm a patient who's prone to fainting at any time.
             </p>
-            <Button text="Get started as Patient" />
+            <Button handles={getStartedForPatient} text="Get started as Patient" />
           </Card>
         </div>
         <div className="w-11/12 2xs:mt-16">
@@ -23,7 +33,7 @@ export default function GetStarted() {
               {" "}
               I'm a caretaker or a person on the emergency list.{" "}
             </p>
-            <Button text="Get started as Caretaker" />
+            <Button handles={getStartedForCaretaker} text="Get started as Caretaker" />
           </Card>
         </div>
       </section>
