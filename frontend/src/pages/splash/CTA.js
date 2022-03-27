f41c316536a71f6caf96f3f3595b80ca5ea72de5
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-03-27 05:00:37 +0200

    Link between splash and getstarted

--- frontend/src/pages/splash/CTA.js
+++ frontend/src/pages/splash/CTA.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Button from "../../components/library/Button";
-
+import { useNavigate } from "react-router-dom";
 export default function CTA() {
+  let navigate = useNavigate();
+  const navigateToGetStarted = () => {
+    console.log("clicked");
+    navigate("/getstarted");
+  };
   return (
     <section className="flex flex-col justify-center items-center mt-8 2xs:mt-16">
-      <Button text="GET STARTED" />
+      <Button text="GET STARTED" handles={navigateToGetStarted} />
       <p className="mt-2">
         <span className="text-sm font-semibold text-prim text-center">
           Already have an account?
