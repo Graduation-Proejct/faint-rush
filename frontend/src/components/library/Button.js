05c792c7ff6a3914b89f3e25a7201120aee01b1f
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-03-27 05:27:13 +0200

    get started screen

--- frontend/src/components/library/Button.js
+++ frontend/src/components/library/Button.js
@@ -4,8 +4,8 @@ export default function Button(props) {
   const { text, handles } = props;
   return (
     <>
-      <button onClick={handles  } className="px-24 py-4 rounded-2xl bg-gradient-to-br from-green-300 to-prim text-white text-sm font-bold">
-        {text}
+      <button onClick={handles  } className="px-24 py-4 rounded-2xl bg-gradient-to-br from-grdstart to-prim text-white text-sm font-bold">
+        <span className="">{text}</span>
       </button>
     </>
   );
--- frontend/src/pages/getstarted/index.js
+++ frontend/src/pages/getstarted/index.js
@@ -8,15 +8,24 @@ export default function GetStarted() {
     <div className="pt-10 h-screen font-mon flex flex-col gap-5 2xs:gap-10 items-center  ">
       <Logo />
       <Header />
-      <section>
-        <Card>
-          I'm a patient who's prone to fainting at any time.
-          <Button text="Get started as Patient" />
-        </Card>
-        <Card>
-          I'm a caretaker or a person on the emergency list.{" "}
-          <Button text="Get started as CareTaker" />
-        </Card>
+      <section className="flex flex-col justify-center items-center mt-20 2xs:mt-32">
+        <div className="w-11/12">
+          <Card>
+            <p className="text-sec font-semibold pb-4">
+              I'm a patient who's prone to fainting at any time.
+            </p>
+            <Button text="Get started as Patient" />
+          </Card>
+        </div>
+        <div className="w-11/12 2xs:mt-16">
+          <Card>
+            <p className="text-sec font-semibold pb-4">
+              {" "}
+              I'm a caretaker or a person on the emergency list.{" "}
+            </p>
+            <Button text="Get started as Caretaker" />
+          </Card>
+        </div>
       </section>
     </div>
   );
