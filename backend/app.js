77d994666278cffae857494f2e026b74238fe48e
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-04-28 05:42:39 +0200

    create a chat app as an example of sockets

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const port = 4545 ; 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(port, () => {
-  console.log('listening on *:' + port);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    socket.broadcast.emit("chat message", msg);
+  });
+});
+
+server.listen(4545, () => {
+  console.log("listening on *:4545");
 });
