9315232272139e37b691eed530b05d6fb9b0e7b6
MohamedKhaled999 <68979528+MohamedKhaled999@users.noreply.github.com>
2022-05-08 20:50:10 +0200

    sos&faint2

--- frontend/src/pages/sos/index.js
+++ frontend/src/pages/sos/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 
+import { useSelector, useDispatch } from 'react-redux'
 
-export default function Faint() {
+export default function SOS() {
+  const user = useSelector((state) => state.user)
   const date = new Date();
   const showTime = date.getHours()
     + ':' + date.getMinutes()
     + ":" + date.getSeconds();
-
   return (
     <div className="flex flex-col items-center ">
-      <div className=" pt-14  pb-6 h-screen font-mon flex flex-col items-center max-w-lg w-full bg-[#F75010]">
-        <span className="block text-white font-bold font[Helvetica] text-[40px]">S.O.S </span>
-        <span className=" pt-10 block text-white font-bold font[Helvetica] text-[23px]">Mahmoud fainted on: {showTime}</span>
+      <div className=" pt-8  pb-6 h-screen font-mon flex flex-col items-center max-w-md w-full bg-[#F75010]">
+        <span className="block text-white font-bold font[Helvetica] text-[40px]">S.O.S</span>
+        <span className=" pt-5 block text-white font-bold font[Helvetica] text-[23px]">Mahmoud fainted on: {showTime}</span>
         <span className="block text-white font-bold font[Helvetica] text-[23px]">(around 30 seconds ago)</span>
 
-        <div className=" ml-6 pt-28 pb-4  flex flex-col items-start gap-2 ">
+        <div className=" ml-6 pt-16 pb-4  flex flex-col items-start gap-2 ">
           <span className=" block pb-2 text-white font-bold font[Helvetica] text-[40px]">Act Quickly</span>
 
           <span className=" text-left text-white font-bold font[Helvetica] text-[23px]">-Call an Ambulance 911</span>
@@ -24,11 +25,11 @@ export default function Faint() {
           <span className="text-left text-white font-bold font[Helvetica] text-[23px]">- Access Mahmoud's medical history</span>
 
         </div>
-        <button className="w-24 h-10 rounded-2xl bg-red-900  text-black font-bold font-mon">Cancel</button>
-  
+        {(user.type == "patient") && 
+        <button className="w-24 h-10 rounded-2xl bg-red-900  text-black font-bold font-mon">Cancel</button>}
 
-        
-        
+
+
 
       </div>
 
