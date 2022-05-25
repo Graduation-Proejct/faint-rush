2b9d4af47edc8da9c68fa1d0f124c88bce30d51a
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-05-25 08:26:26 +0200

    merging socket.io integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
 import "./App.css";
 import "@material-tailwind/react/tailwind.css";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
+//! @MohamedKhaled999 this line is error prone. fix it or consider using an alternative library!
+import "react-toastify/dist/ReactToastify.css";
+
+import { io } from "socket.io-client";
 import { Routes, Route } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 import { lazy, Suspense } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  setEmail,
-  setPassword,
-  setPhone,
-  setType,
-  setUsername,
-  setValid,
-  setList,
-} from "./redux/userSlice";
+
 import PrivateRoute from "./components/PrivateRoute";
 import PrivateRouteSignUp from "./components/PrivateRouteSignUp";
 
@@ -34,23 +29,18 @@ const SOS = lazy(() => import("./pages/sos"));
 
 const Test = lazy(() => import("./pages/test"));
 
-
-
-
-
-
-
 function App() {
-
   const user = useSelector((state) => state.user);
   const navigate = useNavigate();
-
   const dispatch = useDispatch();
-  
 
-
-
-
+  const socket = io("https://faintrush.herokuapp.com/");
+  socket.on("sos-activated", function() {
+    navigate("/sos");
+  });
+  socket.on("faint-alarm", function() {
+    navigate("/faint");
+  });
   return (
     <div className="App">
       <Suspense fallback={<div>Loading...</div>}>
@@ -59,34 +49,59 @@ function App() {
           <Route path="/getstarted" element={<GetStarted />} />
           <Route path="/login" element={<Login />} />
 
-          <Route path="/patienthome" element={
-            <PrivateRoute><PatientHome /></PrivateRoute>
-          } />
+          <Route
+            path="/patienthome"
+            element={
+              <PrivateRoute>
+                <PatientHome />
+              </PrivateRoute>
+            }
+          />
 
-          <Route path="/edit" element={
-            <PrivateRoute><Edit /></PrivateRoute>
-          } />
-          <Route path="/signup" element={
-            <PrivateRouteSignUp><SignUp /></PrivateRouteSignUp>
-          } />
-          <Route path="/signupnext" element={
-            <PrivateRoute><SignUpNext /></PrivateRoute>
-          } />
+          <Route
+            path="/edit"
+            element={
+              <PrivateRoute>
+                <Edit />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <PrivateRouteSignUp>
+                <SignUp />
+              </PrivateRouteSignUp>
+            }
+          />
+          <Route
+            path="/signupnext"
+            element={
+              <PrivateRoute>
+                <SignUpNext />
+              </PrivateRoute>
+            }
+          />
 
-          <Route path="/test" element={
-            <Test />} />
-          <Route path="/faint" element={
-            <Faint />} />
-          <Route path="/sos" element={
-            <PrivateRoute><SOS /></PrivateRoute>
-          } />
+          <Route path="/test" element={<Test />} />
+          <Route path="/faint" element={<Faint />} />
+          <Route
+            path="/sos"
+            element={
+              <PrivateRoute>
+                <SOS />
+              </PrivateRoute>
+            }
+          />
 
-
-
-          <Route path="/caretaker" element={
-            <PrivateRoute><CareTaker /></PrivateRoute>
-          } />
-
+          <Route
+            path="/caretaker"
+            element={
+              <PrivateRoute>
+                <CareTaker />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </Suspense>
       <ToastContainer />
