a35c92b98a1278cc38ed709c19bda72714c65104
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-06-18 15:28:21 +0200

    remove unwanted imports

--- frontend/src/pages/patienthome/index.js
+++ frontend/src/pages/patienthome/index.js
@@ -1,35 +1,28 @@
-import React from "react";
-
-import Header from "./Header";
-import Modal from "../../components/library/Modal";
 import axios from "axios";
+import dd from "../../assets/svgs/takecarer.png";
 import Modal2 from "../../components/library/Modal2";
-import dd from "../../assets/svgs/takecarer.png"
-
-
-
+import Header from "./Header";
 
 //import ListItem from "./ListItem";
-import ListItem from "../../components/library/ListItem";
 import { useNavigate } from "react-router-dom";
+import ListItem from "../../components/library/ListItem";
 
-import { useLocation } from "react-router-dom";
 import Button from "../../components/library/Button";
 
-import { useSelector, useDispatch } from "react-redux";
-import {
-  setEmail,
-  setPassword,
-  addItem,
-  setUsername,
-  setValid,
-  delCareTaker,
-} from "../../redux/userSlice";
-import{  setSignUpValue,setEditValue,setLoading,setList,delItem,setCancel,setShowModel }from"../../redux/counterSlice"
-
+import { useCallback, useContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setShowModel } from "../../redux/counterSlice";
+import { delCareTaker } from "../../redux/userSlice";
+import { SocketContext } from "../../services/Socket";
 
 export default function PatientHome() {
-  
+  const socket = useContext(SocketContext);
+
+  const [ joined, setJoined ] = useState(false);
+
+  const handleInviteAccepted = useCallback(() => {
+    setJoined(true);
+  }, []);
 
   const user = useSelector((state) => state.user);
   const items = useSelector((state) => state.items);
@@ -39,56 +32,61 @@ export default function PatientHome() {
 
   let list = user.list;
   //notify every one in the list
-  const notifyEmergencylist =()=>{
+  const notifyEmergencylist = () => {
     // window.alert("hi mo")
-     navigate("/sos")
-     
-   }
-   
-   const cancel =()=>{
-     dispatch(setShowModel(false))
-    
-   }
+    navigate("/sos");
+  };
 
-  console.log(user.list)
+  const cancel = () => {
+    dispatch(setShowModel(false));
+  };
+
+  console.log(user.list);
   // window.alert(user.list.length);
   // add caretaker to list
   const addTakecarer = () => {
     // window.alert(user.list.length);
 
-    
     //console.log(user.list);
 
     navigate("/edit");
     //console.log(user.list);
   };
 
-  // nev to edit page
-  const PatientEdit =async (item) => {
-    const temp= {UID:user.UID, emailCaretaker:item.email}
-    console.log("dsa"+temp.emailCaretaker)
-    await axios.post("https://faintbaseapp.herokuapp.com/delete_caretaker", temp)
-    .then((response) => {
+  useEffect(
+    () => {
+      return () => {
+        // before the component is destroyed
+        // unbind all event handlers used in this component
+        socket.off("JOIN_REQUEST_ACCEPTED", handleInviteAccepted);
+      };
+    },
+    [ socket, handleInviteAccepted ]
+  );
 
-      console.log("before"+response.data);
-      if(response.data){
-        console.log("after"+response.data);
-        dispatch(delCareTaker(item))
-      }
-     
-    });
+  // nev to edit page
+  const PatientEdit = async (item) => {
+    const temp = { UID: user.UID, emailCaretaker: item.email };
+    console.log("dsa" + temp.emailCaretaker);
+    await axios
+      .post("https://faintbaseapp.herokuapp.com/delete_caretaker", temp)
+      .then((response) => {
+        console.log("before" + response.data);
+        if (response.data) {
+          console.log("after" + response.data);
+          dispatch(delCareTaker(item));
+        }
+      });
     //https://faintbaseapp.herokuapp.com/
-   
+
     //list.splice(state.list.indexOf(action.payload), 1)
 
-   // navigate("/edit", { state: { id: item.id } });
+    // navigate("/edit", { state: { id: item.id } });
     //todo dispatch action to set user type to caretaker
   };
-console.log("sdf"+items.showModelx);
+  console.log("sdf" + items.showModelx);
   return (
     <div className=" h-screen font-mon flex flex-col items-center w-auto gap-5 2xs:gap-10 bg-scroll">
-              
-
       <Header />
 
       {list.length > 0 && (
@@ -113,11 +111,12 @@ console.log("sdf"+items.showModelx);
           text="Add"
           handles={addTakecarer}
           type="button"
-         
           data-modal-toggle="defaultModal"
         />
       )}
-       {(items.showModelx)&& <Modal2 handlesNotify={notifyEmergencylist} handlesCancel={cancel}/>}
+      {items.showModelx && (
+        <Modal2 handlesNotify={notifyEmergencylist} handlesCancel={cancel} />
+      )}
     </div>
   );
 }
