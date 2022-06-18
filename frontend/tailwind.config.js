12ed24dfa19faca0747c74424d0d06ff6495ebe9
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-06-18 18:05:36 +0200

    adding daisy UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [ "./src/**/*.{js,jsx,ts,tsx}" ],
   theme: {
     extend: {
       fontFamily: {
         pop: "'Poppins', 'sans-serif'",
         mon: "'Montserrat', 'sans-serif'",
         new: "'Take Me One', 'sans-serif'",
-        hel:"Helvetica",
+        hel: "Helvetica",
       },
       colors: {
         prim: "#3CAA78",
-        prim2:"#68B2A0",
+        prim2: "#68B2A0",
         sec: "#205072",
         grdstart: "#7BE495",
-        patienthome:"#E0ECDE",
-        crazyblue:"#205072",
+        patienthome: "#E0ECDE",
+        crazyblue: "#205072",
       },
       fontSize: {
         "1.5xl": "1.32rem",
@@ -24,5 +24,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [ require("daisyui") ],
 };
